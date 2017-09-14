300905231c94afaabd2c68c43060e222ed0e1b02
Brandon Eleuterio <eleuterio@railroad19.com>
2017-09-13 21:01:11 -0600

    new player listener

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -1,10 +1,17 @@
 export class GameController {
   constructor() {
-    this.runGameCycle();
+    // this.runGameCycle();
   }
 
   public listen(io: SocketIO.Server) {
     console.log('game controller is listening...');
+
+    io.sockets.on('connection', (socket) => {
+      socket.on('new player', (name, image) => {
+        console.log('new player added:', name);
+this.runGameCycle();
+      });
+    });
   }
 
   private runGameCycle() {
